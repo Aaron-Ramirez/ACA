aeb7bac7eeb993339b5aabdbe8e13be97d155694
Aaron Ramirez <djramaustin@gmail.com>
2017-04-15 21:26:23 -0500

    folio tweaks

--- 10week/portfolio/js/script.js
+++ 10week/portfolio/js/script.js
@@ -1,10 +1,45 @@
 $(document).ready(function(){
+  //hide divs to load later
+  $('.work-hero').hide();
+
+  // runner delay
+  $("#runbtn").click(function () {
+    $('.work-hero').hide();
+    setTimeout(function(){
+      $('.run').fadeIn(1000);
+      $("#runframe").attr("src", "https://aaron-ramirez.github.io/ACA/03week/blog/index.html");
+  }, 1000);
+});
+// yamaha delay
+  $("#yambtn").click(function () {
+    $('.work-hero').hide();
+    setTimeout(function(){
+      $('#yamrow').fadeIn(1000);
+      $("#yamframe").attr("src", "https://aaron-ramirez.github.io/ACA/04week/01Checkpoint/index.html");
+  }, 1000);
+  });
+  // welcome delay
+    $("#welbtn").click(function () {
+      $('.work-hero').hide();
+      setTimeout(function(){
+        $('.welcome').fadeIn(1000);
+        $("#welframe").attr("src", "https://aaron-ramirez.github.io/ACA/04week/welcome/index.html");
+    }, 1000);
+    });
+    // foundry delay
+      $("#drybtn").click(function () {
+        $('.work-hero').hide();
+        setTimeout(function(){
+          $('.dryrow').fadeIn(1000);
+          $("#dryframe").attr("src", "https://aaron-ramirez.github.io/ACA/07week/Checkpoint2/index.html");
+      }, 1000);
+      });
     // tictactoe delay
     $("#toebtn").click(function () {
-      $('.tictac').hide();
+      $('.work-hero').hide();
       setTimeout(function(){
         $('.tictac').fadeIn(1000);
-        $("#toeframe").attr("src", "https://spazboy.github.io/web-workbook/09week/tictactoe/index.html");
+        $("#toeframe").attr("src", "https://aaron-ramirez.github.io/ACA/09week/tictactoe/index.html");
     }, 1000);
   });
 
